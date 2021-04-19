5b26f80b79beec3fd181b2ce156517393a259278
Ashley Barr <ashley.barr@ar4s.com>
2021-04-18 20:08:45 -0600

    fix display bug

--- src/App.js
+++ src/App.js
@@ -5,40 +5,78 @@ import lottery from './lottery';
 
 function App() {
   
-  const [ manager, setManager ] = useState('');
+  // Contract functions && states
+  const [ admin, setAdmin ] = useState('');
   const [ players, setPlayers ] = useState([]);
   const [ balance, setBalance ] = useState('');
   const [ message, setMessage ] = useState('');
 
+  // Set states based on contract instance state(s)
   useEffect(() => {
-    async function asyncCalls() {
-
-      await lottery.methods.manager().call().then(result => setManager(result));
+    const asyncCalls = async () => {
+      await lottery.methods.admin().call().then(result => setAdmin(result));
       await lottery.methods.getPlayers().call().then(result => setPlayers(result));
       await web3.eth.getBalance(lottery.options.address).then(result => setBalance(result));
 
+      // console.log(`admin`, admin);
     }
+ 
     asyncCalls();
   }, []);
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    setMessage('Waiting for the transaction to be mined...');
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('1', 'ether'),
+    });
+    setMessage('You have been entered into the lottery! Good luck!!');
+  };
+
+  const pickWinner = async (e) => {
+    e.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    setMessage('Waiting for the transaction to be mined...');
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+    setMessage('A winner has been picked!');
+  };
+
+  const isAdmin = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts[0]);
+    return accounts[0] === admin;
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lottery Contract</h2>
+      <p>
+        This contract is managed by {admin}.
+        There are currently {players.length} people in the pool,
+        competing to win {web3.utils.fromWei(balance, 'ether')} ether!
+      </p>
+      <hr />
+      <form onSubmit={onSubmit}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Each lottery ticket costs exactly 1 Ether.</label>
+        </div>
+        <button>Enter</button>
+      </form>
+      { isAdmin &&
+        <>
+          <hr />
+          <h4>Ready to pick a winner?</h4>
+          <button onClick={pickWinner}>Pick a winner!</button>
+        </>
+      }
+      <hr />
+      <h2>{message}</h2>
     </div>
   );
 }
+
 
 export default App;
--- src/lottery.js
+++ src/lottery.js
@@ -2,65 +2,71 @@ import web3 from './web3';
 
 const address = '0xF7f72ef2D4726f13D69Dc87715CCa9EeaF4416E8';
 const abi = [
-  {
-    inputs: [],
-    stateMutability: 'nonpayable',
-    type: 'constructor',
-    constant: undefined,
-    payable: undefined,
-    signature: 'constructor'
-  },
-  {
-    inputs: [],
-    name: 'admin',
-    outputs: [ [Object] ],
-    stateMutability: 'view',
-    type: 'function',
-    constant: true,
-    payable: undefined,
-    signature: '0xf851a440'
-  },
-  {
-    inputs: [],
-    name: 'enter',
-    outputs: [],
-    stateMutability: 'payable',
-    type: 'function',
-    constant: undefined,
-    payable: true,
-    signature: '0xe97dcb62'
-  },
-  {
-    inputs: [],
-    name: 'getPlayers',
-    outputs: [ [Object] ],
-    stateMutability: 'view',
-    type: 'function',
-    constant: true,
-    payable: undefined,
-    signature: '0x8b5b9ccc'
-  },
-  {
-    inputs: [],
-    name: 'pickWinner',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-    constant: undefined,
-    payable: undefined,
-    signature: '0x5d495aea'
-  },
-  {
-    inputs: [ [Object] ],
-    name: 'players',
-    outputs: [ [Object] ],
-    stateMutability: 'view',
-    type: 'function',
-    constant: true,
-    payable: undefined,
-    signature: '0xf71d96cb'
-  }
-  ];
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "admin",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "enter",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getPlayers",
+		"outputs": [
+			{
+				"internalType": "address[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "pickWinner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "players",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
 
 // Create local copy of contract
 export default new web3.eth.Contract(abi, address);
